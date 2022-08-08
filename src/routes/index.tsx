2ab56f9690f3ce77061e171139a6ec9615f1e5db
zellytozelly <zellytozelly@gmail.com>
2022-08-08 23:55:51 +0900

    Feat: Add login frame

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import styles from './routes.module.scss'
 
 import Main from 'pages/Main'
+import Login from 'pages/Login'
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='login' element={<Login />} />
         <Route path='*' element={<p>404 Not Found</p>} />
       </Routes>
     </div>
