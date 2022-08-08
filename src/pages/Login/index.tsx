650df9be9a914fcfb70937ee9acc249ab46deea7
zellytozelly <zellytozelly@gmail.com>
2022-08-08 23:55:51 +0900

    Feat: Add login frame

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import styles from './login.module.scss'
+
+const Login = () => {
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.whiteBox}>
+        <p>컨테이너</p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
